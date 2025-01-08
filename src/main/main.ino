037ac14e7a168ca3bed3764b9f4aed1d66f1c619
Anis Ziadi <med.anis.ziadi@gmail.com>
2025-01-08 18:33:59 +0100

    Use Porrey library  for max1704x

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,11 +1,38 @@
+#include "Wire.h"
+#include "MAX17043.h"
+
 void setup() {
   // put your setup code here, to run once:
   // Initialisation de la console série
   Serial.begin(9600); // Baud rate de 115200 (courant pour ESP8266)
   
+  //
+
   // Attendre que la console soit prête
   delay(1000); 
   Serial.println("ESP8266 démarré !");
+
+ Serial.println("Recherche des appareils I2C...");
+  /*
+  Wire.begin();
+  for (byte i = 8; i < 120; i++) {
+    Wire.beginTransmission(i);
+    if (Wire.endTransmission() == 0) {
+      Serial.print("Appareil trouvé à l'adresse 0x");
+      Serial.println(i, HEX);
+    }
+  }
+  */
+
+  Wire.begin(SDA, SCL);
+  if (!FuelGauge.begin(&Wire)) {
+    Serial.println("MAX17043 NOT found.\n");
+    while (true) {}
+  }
+  FuelGauge.reset();
+  delay(250);
+  FuelGauge.quickstart();
+  delay(125);
 
 }
 
@@ -14,4 +41,11 @@ void loop() {
 
   // Attendre 1 seconde avant de répéter
   delay(1000);
+
+  float volts = FuelGauge.voltage();
+  float pcnt = FuelGauge.percent();
+  Serial.printf("%.0fmV (%.1f%%)\n", volts, pcnt);
+  delay(3000);
+
+ 
 }
